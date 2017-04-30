7293c05df3e934b6a338d0f2e08e1512b0095d56
natasha <paranyak@ucu.edu.ua>
2017-04-30 15:19:38 +0300

    Added folder with detection results

--- example_color_HSV.py
+++ example_color_HSV.py
@@ -1,12 +1,14 @@
 # import the necessary packages
 import glob
-
 import numpy as np
 import cv2
+import os
+from os.path import basename
+
 
 for infile in glob.glob("I/*"):
     im = cv2.imread(infile)
-
+    file, ext = os.path.splitext(infile)
     # define the upper and lower boundaries of the HSV pixel
     # intensities to be considered 'skin'
     lower = np.array([0, 48, 36], dtype="uint8")
@@ -27,6 +29,9 @@ for infile in glob.glob("I/*"):
     skinMask = cv2.GaussianBlur(skinMask, (3, 3), 0)
     skin = cv2.bitwise_and(im, im, mask=skinMask)
 
-    # show the skin in the image along with the mask
-    cv2.imshow("images", np.hstack([im, skin]))
-    cv2.waitKey(0)
+    #saving images in folder I_Detection
+    os.chdir("./I_Detection")
+    name = os.path.splitext(basename(infile))[0] + "_HSV" + ext
+    print(name)
+    cv2.imwrite(name,  skin)
+    os.chdir("..")
